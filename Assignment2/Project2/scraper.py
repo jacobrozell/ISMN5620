ce4fb09fc84aac3dc4a8945e9532f8c826d1a5f0
Jacob Rozell <jacob.rozell83@gmail.com>
2020-01-12 22:52:37 -0600

    finish bonus on assignment 2

--- Assignment2/Project2/scraper.py
+++ Assignment2/Project2/scraper.py
@@ -1,15 +1,18 @@
 from bs4 import BeautifulSoup
 import ssl
 import json
+import csv
 from urllib.request import Request, urlopen
 
+allVideos = {}
 
-if __name__== '__main__':
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
 
-    url = input("Enter YouTube Video URL: ")
+def parse_video_from_url(url, iterationNum=0):
+    global allVideos
+
+    iteration = "" if iterationNum == 0 else " #" + str(iterationNum+1)
+
+    print("Examining Video" + iteration)
 
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
@@ -22,7 +25,7 @@ if __name__== '__main__':
 
     for script in soup.findAll('script', attrs={'type': 'application/ld+json'}):
         channelDescription = json.loads(script.text.strip())
-        video_details['CHANNEL_NAME'] =channelDescription['itemListElement'][0]['item']['name']
+        video_details['CHANNEL_NAME'] = channelDescription['itemListElement'][0]['item']['name']
 
     for div in soup.findAll('div', attrs={'class': 'watch-view-count'}):
         video_details['NUMBER_OF_VIEWS'] = div.text.strip()
@@ -37,9 +40,67 @@ if __name__== '__main__':
         video_details['NUMBER_OF_SUBSCRIPTIONS'] = span.text.strip()
 
     with open('data.json', 'w', encoding='utf8') as outfile:
-        json.dump(video_details, outfile, ensure_ascii=False, indent=4)
+        # json.dump(video_details, outfile, ensure_ascii=False, indent=4)
+        allVideos[iterationNum] = video_details
+        json.dump(allVideos, outfile, ensure_ascii=False, indent=4)
 
-    print('------Extraction of data is complete. Check json file.------')
+    print('------Extraction of data is complete. Check json file.------\n')
 
-    # DO THE BONUS
+
+def read_json_file():
+    with open('data.json') as fp:
+        line = fp.readline()
+        while line:
+            print(line)
+            line = fp.readline()
+
+
+def csv_to_array(file_path):
+    with open(file_path, newline='') as csvfile:
+        data = list(csv.reader(csvfile))
+    return data
+
+
+def exit_options():
+    ans = input("\nDo you want to:\n1) Print out JSON file\n2) Exit Program\n~")
+    if ans == "1":
+        read_json_file()
+    else:
+        print("Exiting Program.")
+        quit()
+
+
+if __name__ == '__main__':
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    version = input("\nDo you want to:\n1) Enter a YouTube URL\n2) Enter a .CSV of YouTube URLS\n~")
+
+    notSet = True
+    while notSet:
+        if version == "1":
+            notSet = False
+            url = input("\nEnter YouTube Video URL: ")
+
+            parse_video_from_url(url)
+            exit_options()
+
+        elif version == "2":
+            notSet = False
+
+            path = input("\nEnter CSV file path: ")
+
+            array = csv_to_array(path)
+
+            count = 0
+            for subArray in array:
+                for url in subArray:
+                    parse_video_from_url(url, count)
+                    count += 1
+
+            exit_options()
+
+        else:
+            print("Try again")
 
